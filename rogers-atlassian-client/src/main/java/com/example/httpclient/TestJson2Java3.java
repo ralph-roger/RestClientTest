83c4680355b1ebfeb2439ed8faa436e0fee7a9bd
Ralph-Roger <ralph_reifegerste@web.de>
2019-11-29 16:40:38 +0100

    Next Step to List  auf Objekts

--- rogers-atlassian-client/src/main/java/com/example/httpclient/TestJson2Java3.java
+++ rogers-atlassian-client/src/main/java/com/example/httpclient/TestJson2Java3.java
@@ -5,22 +5,18 @@ import java.util.List;
 import com.google.api.client.util.Key;
 import com.google.gson.Gson;
 
-public class TestJson2Java2 {
+public class TestJson2Java3 {
 
 	public static void main(String... args) throws Exception {
-		String json = "{'title' : 'Erste Tests mit gson', 'id' : 1, 'children' : 'true', "
-				+ "'groups' : [{'name' : 'Reifegerste', 'vname' : 'Ralph-Roger'},"
-				+ "{'name' : 'Rössig', 'vname' : 'Ulrike'}]}";
+		String json = "{'title' : 'Computing and Information systems', 'id' : 1, 'children' : 'true', 'groups' : {'name' : 'Reifegerste', 'vname' : 'Ralph-Roger'}}";
 
 		// Now do the magic.
 		Data data = new Gson().fromJson(json, Data.class);
 
 		// Show it.
 		System.out.println("Test ---> " + data);
-		for (Data1 data1 : data.getGroups()) {
-			System.out.println(data1.getVname() + " " + data1.getName());
-		}
-
+		System.out.println("Test ---> " + data.getGroups().getName());
+		
 	}
 
 	class Data {
@@ -31,7 +27,7 @@ public class TestJson2Java2 {
 		@Key
 		private Boolean children;
 		@Key
-		private List<Data1> groups;
+		private Data1 groups;
 
 		public String getTitle() {
 			return title;
@@ -45,7 +41,7 @@ public class TestJson2Java2 {
 			return children;
 		}
 
-		public List<Data1> getGroups() {
+		public Data1 getGroups() {
 			return groups;
 		}
 
@@ -61,7 +57,7 @@ public class TestJson2Java2 {
 			this.children = children;
 		}
 
-		public void setGroups(List<Data1> groups) {
+		public void setGroups(Data1 groups) {
 			this.groups = groups;
 		}
 
@@ -69,29 +65,23 @@ public class TestJson2Java2 {
 			return String.format("title:%s,id:%d,children:%s,groups:%s", title, id, children, groups);
 		}
 	}
-
 	class Data1 {
 		@Key
 		String name;
 		@Key
 		String vname;
-
 		public String getName() {
 			return name;
 		}
-
 		public void setName(String name) {
 			this.name = name;
 		}
-
 		public String getVname() {
 			return vname;
 		}
-
 		public void setVname(String vname) {
 			this.vname = vname;
 		}
-
 		public String toString() {
 			return String.format("Name ist " + vname + " " + name);
 		}
